1ace35c64d5a60c9e5a37fc560a5ef4ac5ce2fe9
SeriousCoffeeCup <seriouscoffeecup@gmail.com>
2024-07-02 19:36:36 -0700

    Quicksave

--- mario_number_predicter.py
+++ mario_number_predicter.py
@@ -68,40 +68,73 @@ def main():
     def tester_permu_multiset():
         print("permu_multiset")
         total_space = 7
+        # max_possible_steps = total_space - 1 
+        
         for n in range(total_space + 1):
             print(f"n {n}")
-            max_j = max_jumps(n)
+            max_j = max_jumps(n) # This is kinda roundabout and not soclearly correct
             for j_num in range(max_j + 1):
                 steps = n - ((2*j_num) +1)
                 print(f"Space: {n},  Jumps: {j_num}, Steps: {steps} : {permu_multiset(j_num, steps)}")
-    # tester_permu_multiset()        
+    tester_permu_multiset()        
         
     
     def pure_plains_mario_number_predicter(total_space = 7):
         paths = 0
-        for n in range(total_space + 1):
-            print(f"n {n}")
-            max_j = max_jumps(n)
-            for j_num in range(max_j + 1):
-                steps = n - ((2*j_num) +1)
-                
-                orderings = permu_multiset(j_num, steps)
-                
-                print(f"Space: {n},  Jumps: {j_num}, Steps: {steps} : {orderings}")
-                paths += orderings
-
+        max_j = max_jumps(total_space)
+        for j_num in range(max_j + 1):
+            steps = total_space - ((2*j_num) +1)
+            
+            orderings = permu_multiset(j_num, steps)
+            paths += orderings
+            print(f"Space: {total_space},  Jumps: {j_num}, Steps: {steps} : Orderings {orderings}")
+            
+        print(f"Paths: {paths}")
         return paths
     
-    print("Mario Numbers")
-    # for n in range(8):
-    check = 8
-    print(f"{check} : {pure_plains_mario_number_predicter(check)}")
+    def test_plains():
+        print("Mario Numbers")
+        # for n in range(total_space + 1):
+        space = 4
+        for n in range(space + 1):
+            print(f"{n} : {pure_plains_mario_number_predicter(n)}")
+        
+    def readable_level(level):
+        # g for grass
+        return level.replace(' ', 'g')
+    
+    def tester_mario():
+        parameters = list()
+        
+        # len_0 = [('', 1)]
+        # parameters += len_0
+        
+        # illegal_P = [('P  ', 0), ('  P', 0), ('  PP ', 0) ]
+        # parameters += illegal_P
+        
+        case3 = ('   P ', 2)
+        # Case 3 variations:
+        # step step jump _ _ 
+        # or 
+        # jump _ jump _ _
+        parameters += [ (' P ', 1), ('   ', 2), case3]
+        
+        provided_doctests = [(' P P ', 1), (' P P  ', 1), ('  P P ', 1), ('   P P ', 2), (' P PP ', 0), ('    ', 3), ('    P    ', 9), ('   P    P P   P  P P    P     P ', 180)]
+        parameters += provided_doctests
+
+        # for index, p in enumerate(parameters):
+            # print(f"{index} : n : {p} : {mario_number(p[0])} : Expected: {p[1]}")
+            # print(f"{index} : {readable_level(p[0])} : Original: '{p[0]}'")
+            # spaces = len(p[0])
+            # print(f"{index} : {readable_level(p[0])} : {pure_plains_mario_number_predicter(spaces)} : Expected: {p[1]}")
+    # tester_mario()
+    # print(pure_plains_mario_number_predicter(4))
 
 if __name__ == "__main__":
     main()
     
 """
-G = grass aka Space
+g or G = grass aka Space
 s or S = step 
 P = plan
 J = jump, which only works on GPG
@@ -177,5 +210,22 @@ GGGGGGG
     SSJSS
     SSSJS
     SSSSJ
-    
+
+
+GG GG GG GG 
+    JJSSS
+    JJSJS
+    JJSSJ
+    JSJJS
+    JSJSJ
+    JSSJJ
+    SJJJS
+    SJJSS
+    SJSJJ
+    SSJJS
+
+GG GG P GG GG 
+SSS J SSS
+
+
 """
